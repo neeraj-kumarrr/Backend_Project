3cba60c89831e7d141018e412b4a72ae44d2d221
Neeraj <ng16233@gmail.com>
2025-07-27 02:45:18 +0530

    first commit added backend work

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+// import mongoose from "mongoose";
+
+export const DB_NAME = "Todos"
---
+++ src/db/conn.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js"
+
+
+ const connectDB = async ()=>{
+        try {
+            const mongo_instance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+            
+            console.log("Mongodb Connected" , mongo_instance.connection.host)
+        } catch (error){
+            console.error("error is" , error)
+            process.exit(1)
+        }
+        };
+        // connectDB()
+
+    export default connectDB; 
